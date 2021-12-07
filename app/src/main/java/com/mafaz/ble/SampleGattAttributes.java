d8c89357124c58c32da406de762706dd426ec84c
OwaisBukhari <owaisbukhari257@gmail.com>
2021-12-07 19:15:05 +0500

    ONLY for temp sensor

---
+++ app/src/main/java/com/mafaz/ble/SampleGattAttributes.java
@@ -0,0 +1,26 @@
+package com.mafaz.ble;
+
+
+import java.util.HashMap;
+
+/**
+ * Created by brijesh on 15/4/17.
+ */
+
+public class SampleGattAttributes {
+    public static final String UUID_BATTERY_SERVICE = "0000ffc6-0000-1000-8000-00805f9b34fb";
+    public static final String UUID_BATTERY_LEVEL_UUID = "0000ffeb-0000-1000-8000-00805f9b34fb";
+
+    private static HashMap<String, String> attributes = new HashMap();
+
+    static {
+        attributes.put(UUID_BATTERY_LEVEL_UUID, "Battery Level");
+        attributes.put(UUID_BATTERY_SERVICE, "Battery Service");
+    }
+
+    public static String lookup(String uuid) {
+        String name = attributes.get(uuid);
+        return name;
+    }
+}
+
